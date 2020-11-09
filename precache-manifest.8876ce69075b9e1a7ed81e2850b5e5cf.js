6d37605e0d9ef7a3e364d44672f36e906aec880c
Niels Gregers Johansen <niels@timeboxer.dk>
2020-11-09 12:10:24 +0100

    Updates

---
+++ precache-manifest.8876ce69075b9e1a7ed81e2850b5e5cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "960e50b604d519b21a704709adc33704",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4150a8c0545e289b8143",
+    "url": "/static/css/2.043a084f.chunk.css"
+  },
+  {
+    "revision": "ae8dd9ab1ef224b77bf7",
+    "url": "/static/css/main.97f67e66.chunk.css"
+  },
+  {
+    "revision": "4150a8c0545e289b8143",
+    "url": "/static/js/2.a1fef5db.chunk.js"
+  },
+  {
+    "revision": "ae8dd9ab1ef224b77bf7",
+    "url": "/static/js/main.376f4845.chunk.js"
+  },
+  {
+    "revision": "dda29e4e3ab46290e95b",
+    "url": "/static/js/runtime-main.69db3f47.js"
+  }
+]);
