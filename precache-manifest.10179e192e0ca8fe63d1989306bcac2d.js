6bbcbe4cbbf84fa7d17fc43741a89a132d1057ea
NielsGregers <niels@jumpto365.com>
2020-11-11 11:43:08 +0100

    Updates

--- precache-manifest.10179e192e0ca8fe63d1989306bcac2d.js
+++ precache-manifest.10179e192e0ca8fe63d1989306bcac2d.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7f3ca626998cb318a8a3f2e521c7c639",
+    "revision": "6da82738f1fd79af24504ace53caadb1",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.043a084f.chunk.css"
   },
   {
-    "revision": "186e27fa34c0e5594659",
+    "revision": "1a4e5a57ce822e9fdd65",
     "url": "/static/css/main.42491bb1.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.f6c93f7e.chunk.js"
   },
   {
-    "revision": "186e27fa34c0e5594659",
-    "url": "/static/js/main.ca0310b5.chunk.js"
+    "revision": "1a4e5a57ce822e9fdd65",
+    "url": "/static/js/main.dd736005.chunk.js"
   },
   {
     "revision": "dda29e4e3ab46290e95b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dbc16a08df15fcc8ac832c8cf92e3014.js"
+  "/precache-manifest.10179e192e0ca8fe63d1989306bcac2d.js"
 );
 
 self.addEventListener('message', (event) => {
